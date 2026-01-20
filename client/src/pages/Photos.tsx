ca02866594cea22b53525f53f408138a15a70803
Khushboo <khushboo6898patel@gmail.com>
2026-01-21 01:08:58 +0530

    update baarat card image

--- client/src/pages/Photos.tsx
+++ client/src/pages/Photos.tsx
@@ -10,7 +10,7 @@ import celebrationDancing from "@assets/generated_images/Wedding_celebration_dan
 import receptionTable from "@assets/generated_images/Reception_table_setting_9a6a559f.png";
 import haldiCeremony from "@assets/generated_images/Haldi.png";
 import sangeetStage from "@assets/generated_images/sangeet.png";
-import baaratHorse from "@assets/generated_images/Baarat_horse_procession_1afc7c90.png";
+import baaratHorse from "@assets/generated_images/baarat.png";
 import engagement from "@assets/generated_images/497d05e1-4cdf-4ab2-9d6c-385b3800df85.JPG";
 import engagement1 from "@assets/generated_images/af26403a-e803-4c4c-9cb8-b9385c655ba0.JPG";
 
